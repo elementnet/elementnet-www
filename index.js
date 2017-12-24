fcf7dbeec880e194fbd0cb6b5efcd223d594b1d8
MegaCode18 <mmszek3000@gmail.com>
2017-12-24 15:22:43 -0500

    setup

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-var StaticServer = require('static-server');
-var server = new StaticServer({
-  rootPath: '.',
-  port: 8080
-});
-
-server.start(function () {
-  console.log('Server listening to', server.port);
-});
+var http = require('http');
+var fs = require('fs');
+var hash = require('string-hash');

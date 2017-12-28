51959f4115d663e011df3ac3e5e63fb639fea667
Matt <mmszek3000@gmail.com>
2017-12-28 15:30:28 -0500

    Add 404 page to test server

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var StaticServer = require('static-server');
 var server = new StaticServer({
   rootPath: '.',
-  port: 7878
+  port: 7878,
+  templates: {
+    notFound: "404.html"
+  }
 });
 
 server.start(function () {

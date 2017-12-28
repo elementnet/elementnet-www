969dbab1311221b2ad5a9f4b8f967c341ea4d895
Matt <mmszek3000@gmail.com>
2017-12-28 18:37:00 -0500

    dang

---
+++ index.js
@@ -0,0 +1,11 @@
+var StaticServer = require('static-server');
+const PORT = process.env.PORT || 5000
+var server = new StaticServer({
+  rootPath: '.',
+  port: PORT,
+  templates: {notFound: '404.html'}
+});
+
+server.start(function () {
+  console.log('Server listening to', server.port);
+});

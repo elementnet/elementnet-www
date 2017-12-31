684f327823b5918a71fceada508de598f0ba94eb
Matt <mmszek3000@gmail.com>
2017-12-31 17:02:03 -0500

    Actually that made no sense

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 /* eslint no-magic-numbers: "off" */
 var request = require('request');
 var test = require('tape');
-test('test server', function(t) {
+test('test server', (t) => {
     t.plan(3);
-    request('http://localhost:5000/projects/editor/', function(error, response, body) {
+    request('http://localhost:5000/projects/editor/', (error, response, body) => {
         t.false(error);
         t.equal(response.statusCode, 200);
         t.notEqual(body.indexOf('New...'), -1);

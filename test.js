0457470a200f18ee0aa1e9b002e8d9b4ed351ee8
Matt <mmszek3000@gmail.com>
2017-12-31 16:51:45 -0500

    Make test compatible with non-es6 node versions

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 /* eslint no-magic-numbers: "off" */
 var request = require('request');
 var test = require('tape');
-test('test server', (t) => {
+test('test server', function(t) {
     t.plan(3);
-    request('http://localhost:5000/projects/editor/', (error, response, body) => {
+    request('http://localhost:5000/projects/editor/', function(error, response, body) {
         t.false(error);
         t.equal(response.statusCode, 200);
         t.notEqual(body.indexOf('New...'), -1);
